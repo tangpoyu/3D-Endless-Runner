152944db3d615f29fde044e1de93bac018b567d0
boyu <b10623041@gemail.yuntech.edu.tw>
2022-08-15 00:42:44 +0800

    closes #1, closes #2; Coding Player's Movement and Restricting the player move horizontally on the Level

---
+++ My project/Assets/Scripts/Environment/LevelBoundary.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundary : MonoBehaviour
+{
+    [SerializeField]
+    private static float side = 4.57f;
+
+    public static float Side { get => side; set => side = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed, horizontalSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        movement.z = Time.deltaTime * moveSpeed;
+        print(Input.GetAxisRaw(Axis.HORIZONTAL));
+        float horizontalMove = Input.GetAxisRaw(Axis.HORIZONTAL);
+        float currentPosition = transform.position.x;
+        if (horizontalMove > 0 && currentPosition <= LevelBoundary.Side)
+        {
+           movement.x = Time.deltaTime * horizontalSpeed;
+        }
+
+        if (horizontalMove < 0 && currentPosition > -LevelBoundary.Side)
+        { 
+            movement.x = -Time.deltaTime * horizontalSpeed;
+        }
+        transform.Translate(movement);
+    }
+}
